2ada66a8928a6470805db7c66a927eca22b2b851
Hugo Cuenot <hugocuenot@live.fr>
2024-05-01 18:10:56 +0200

    header créée

--- src/view/header.php
+++ src/view/header.php
@@ -4,16 +4,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css" media="all">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/styles.css" media="all">
+    <title>Header</title>
 </head>
 
 <body>
-    <ul>
-        <li><a href="actualites.php">Actualités</a></li>
-        <li><a href="connexion.php"></a>Connexion</li>
-        <li><a href="inscription.php"></a>Inscription</li>
-    </ul>
+    <div class="header">
+        <ul>
+            <li><a href="actualites.php">Actualités</a></li>
+            <li><a href="connexion.php">Connexion</a></li>
+            <li><a href="inscription.php">Inscription</a></li>
+        </ul>
+    </div>
 </body>
 
 </html>
