fd4c6224557959fa402942b6a4fb91d1cf7acb0a
Hugo Cuenot <hugocuenot@live.fr>
2024-05-01 17:42:27 +0200

    debut creation header

---
+++ src/view/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css" media="all">
+    <title>Document</title>
+</head>
+
+<body>
+    <ul>
+        <li><a href="actualites.php">Actualités</a></li>
+        <li><a href="connexion.php"></a>Connexion</li>
+        <li><a href="inscription.php"></a>Inscription</li>
+    </ul>
+</body>
+
+</html>
