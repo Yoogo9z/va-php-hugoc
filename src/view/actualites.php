671cd5ea025e4f03380130503c78043807e85bd3
Hugo Cnt <113591086+Yoogo9z@users.noreply.github.com>
2024-05-02 16:42:43 +0200

    Cartes dynamiques sur l'accueil

--- src/view/actualites.php
+++ src/view/actualites.php
@@ -1,21 +1,35 @@
-<?php
-
+<?php 
 include("header.php");
+require_once("../../bootstrap.php");
+
+// Obtention du repository pour l'entité Actualite
+$repository = $entityManager->getRepository('App\Classes\Actualite');
+
+// Récupération de toutes les actualités avec Doctrine ORM
+$actualites = $repository->findAll();
+
+// Affichage des actualités
 ?>
+
 <h1>ACTUALITÉS</h1>
 
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-4">
-            <div class="card custom-card wd-5">
-                <img src="https://via.placeholder.com/150" class="card-img-top" alt="..." style="width: 348px; height: 300px;">
-                <div class="card-body">
-                    <h5 class="card-title">Titre</h5>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam quis ante convallis, ullamcorper leo ut, tempor dui.</p>
-                    <a href="#" class="btn btn-primary">Bouton</a>
+    <?php foreach ($actualites as $actualite) { ?>
+        <!-- <div class="row justify-content-center"> -->
+            <!-- <div class="col-md-4"> -->
+                <div class="card custom-card wd-2">
+                    <img src="<?php echo $actualite->getImageUrl(); ?>" class="card-img-top img-fluid" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $actualite->getTitre(); ?> </h5>
+                        <p class="card-text"><?php echo $actualite->getContenu(); ?> </p>
+                        <?php if (!empty($actualite->getAuteur())) { ?>
+                            <p class="card-text"><small class="text-muted">Auteur: <?php echo $actualite->getAuteur(); ?></small></p>
+                        <?php } ?>
+                        <p class="card-text"><small class="text-muted">Publié le <?php echo $actualite->getDatePublication(); ?></small></p>
+                        <a href="#" class="btn btn-primary">Bouton</a>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </div>
+            <!-- </div> -->
+        <!-- </div> -->
+    <?php } ?>
 </div>
-
